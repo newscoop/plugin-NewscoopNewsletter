5426139e90430ff06685ffb312b39c286de97185
Rafał Muszyński <rmuszynski1@gmail.com>
2013-12-11 14:06:11 +0100

    lifecycle subscriber fix

--- EventListener/LifecycleSubscriber.php
+++ EventListener/LifecycleSubscriber.php
@@ -56,9 +56,9 @@ class LifecycleSubscriber implements EventSubscriberInterface
     public static function getSubscribedEvents()
     {
         return array(
-            'plugin.install.newscoop_newsletter_plugin' => array('install', 1),
-            'plugin.update.newscoop_newsletter_plugin' => array('update', 1),
-            'plugin.remove.newscoop_newsletter_plugin' => array('remove', 1),
+            'plugin.install.newscoop_newsletter_plugin_bundle' => array('install', 1),
+            'plugin.update.newscoop_newsletter_plugin_bundle' => array('update', 1),
+            'plugin.remove.newscoop_newsletter_plugin_bundle' => array('remove', 1),
         );
     }
 
